f06177f74a6e7af96620b9a4c83b73b5f9fe8f28
Adwin Ying <adwinying@gmail.com>
2017-06-17 12:29:26 +0900

    Check session working in client side

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 
 export default {
   fetchBooks() {
+    Vue.http.post('/api/auth/signin', {
+      username: 'adwin',
+      password: 'secret',
+    })
+      .then((res) => {
+        console.log(res.data)
+        Vue.http.get('/api/auth/profile')
+          .then((profRes) => {
+            console.log(profRes.data)
+          })
+      })
+
+
     Vue.http.get('/api/book/all')
       .then((res) => {
         console.log(res)
